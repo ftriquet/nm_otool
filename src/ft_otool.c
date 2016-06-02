d159cbeb7f83a4becdb0346c24a9740b669adc4d
Alex KPENOU <akpenou@e3r9p14.42.fr>
2016-06-02 22:05:01 +0200

    norm sur otool et fichier test

--- src/ft_otool.c
+++ src/ft_otool.c
@@ -6,7 +6,7 @@
 /*   By: akpenou <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/31 15:50:38 by akpenou           #+#    #+#             */
-/*   Updated: 2016/06/02 21:09:56 by akpenou          ###   ########.fr       */
+/*   Updated: 2016/06/02 22:00:19 by akpenou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-static void ft_print_output_64(struct section_64 *section, void *ptr)
+static void	ft_print_output_64(struct section_64 *section, void *ptr)
 {
 	unsigned char	*p;
 	unsigned int	i;
@@ -28,7 +29,6 @@ static void ft_print_output_64(struct section_64 *section, void *ptr)
 	printf("(%s,%s) section\n", section->segname, section->sectname);
 	p = (void *)ptr + section->offset;
 	i = 0;
-
 	while (i < section->size)
 	{
 		printf("%016llx ", (unsigned long long)section->addr + i);
@@ -47,18 +47,18 @@ void		handler_64(char *ptr)
 	struct load_command			*lc;
 
 	i = 0;
-	lc = (void *) ptr + sizeof(struct mach_header_64);
+	lc = (void *)ptr + sizeof(struct mach_header_64);
 	while (++i < ((struct mach_header_64 *)ptr)->ncmds)
 		if (lc->cmd == LC_SEGMENT_64)
 		{
-			seg = (struct segment_command_64 *) lc;
+			seg = (struct segment_command_64 *)lc;
 			if (!strcmp(seg->segname, SEG_TEXT) && (i = -1))
 			{
 				sect = (void *)seg + sizeof(struct segment_command_64);
 				while (++i < seg->nsects)
 				{
 					if (!strcmp(sect->sectname, SECT_TEXT))
-						return ft_print_output_64(sect, ptr);
+						return (ft_print_output_64(sect, ptr));
 					sect = (void *)sect + sizeof(*sect);
 				}
 			}
@@ -66,7 +66,7 @@ void		handler_64(char *ptr)
 		}
 }
 
-static void ft_print_output_32(struct section *section, void *ptr)
+static void	ft_print_output_32(struct section *section, void *ptr)
 {
 	unsigned char	*p;
 	unsigned int	i;
@@ -75,7 +75,6 @@ static void ft_print_output_32(struct section *section, void *ptr)
 	printf("(%s,%s) section\n", section->segname, section->sectname);
 	p = (void *)ptr + section->offset;
 	i = 0;
-
 	while (i < section->size)
 	{
 		printf("%016llx ", (unsigned long long)section->addr + i);
@@ -94,18 +93,18 @@ void		handler_32(char *ptr)
 	struct load_command		*lc;
 
 	i = 0;
-	lc = (void *) ptr + sizeof(struct mach_header_64);
+	lc = (void *)ptr + sizeof(struct mach_header_64);
 	while (++i < ((struct mach_header *)ptr)->ncmds)
 		if (lc->cmd == LC_SEGMENT)
 		{
-			seg = (struct segment_command *) lc;
+			seg = (struct segment_command *)lc;
 			if (!strcmp(seg->segname, SEG_TEXT) && (i = -1))
 			{
 				sect = (void *)seg + sizeof(struct segment_command_64);
 				while (++i < seg->nsects)
 				{
 					if (!strcmp(sect->sectname, SECT_TEXT))
-						return ft_print_output_32(sect, ptr);
+						return (ft_print_output_32(sect, ptr));
 					sect = (void *)sect + sizeof(*sect);
 				}
 			}
@@ -117,51 +116,56 @@ void		ft_otool(char *ptr)
 {
 	unsigned int	magic_number;
 
-	magic_number = *(int *) ptr;
+	magic_number = *(int *)ptr;
 	if (magic_number == MH_MAGIC_64)
 		handler_64(ptr);
 	else if (magic_number == MH_MAGIC)
 		handler_32(ptr);
 }
 
-int 		main(int argc, char **argv)
+int			ft_openfile(int fd)
 {
-	int				fd;
 	char			*ptr;
 	struct stat		buf;
 
-	fd = 0;
-	if (argc == 1 && (fd = open("a.out", O_RDONLY)) < 0)
+	if (fstat(fd, &buf) < 0 || (ptr = mmap(0, buf.st_size, PROT_READ,
+				MAP_PRIVATE, fd, 0)) == MAP_FAILED)
 	{
-		puts("no arg");
-		return (-1);
+		printf("system failed");
+		return (EXIT_FAILURE);
 	}
-	else if (argc > 2)
-	{
-		puts("too much arg");
-		return (-1);
-	}
-	else if (argc != 1 && (fd = open(argv[1], O_RDONLY)) < 0)
-	{
-		puts("open failed");
-		return (-1);
-	}
-	if (fstat(fd, &buf) < 0)
-	{
-		puts("fstat failed");
-		return (-1);
-	}
-	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
-	{
-		puts("mmap failed");
-		return (-1);
-	}
-	printf("%s:\n", argv[1]);
 	ft_otool(ptr);
 	if (munmap(ptr, buf.st_size) < 0)
 	{
-		puts("munmap failed");
-		return (-1);
+		printf("munmap failed");
+		return (EXIT_FAILURE);
 	}
-	return (0);
+	close(fd);
+	return (EXIT_SUCCESS);
+}
+
+int			main(int argc, char **argv)
+{
+	int				fd;
+	int				i;
+
+	fd = 0;
+	i = 0;
+	if (argc == 1)
+	{
+		printf("Usage %s <files>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	while (++i < argc)
+	{
+		printf("%s:\n", argv[i]);
+		if ((fd = open(argv[1], O_RDONLY)) < 0)
+		{
+			puts("open failed");
+			return (EXIT_FAILURE);
+		}
+		if ((fd = ft_openfile(fd)))
+			return (fd);
+	}
+	return (EXIT_SUCCESS);
 }
