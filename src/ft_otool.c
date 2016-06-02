a9a34eef07e9566b4476a3610b687d8e6ac7250a
Alex KPENOU <akpenou@e3r9p14.42.fr>
2016-06-02 21:09:27 +0200

    norm

--- src/ft_otool.c
+++ src/ft_otool.c
@@ -6,7 +6,7 @@
 /*   By: akpenou <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/31 15:50:38 by akpenou           #+#    #+#             */
-/*   Updated: 2016/06/02 14:58:44 by akpenou          ###   ########.fr       */
+/*   Updated: 2016/06/02 21:08:52 by akpenou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void ft_print_output(struct section_64 *section, void *ptr)
+static void ft_print_output_64(struct section_64 *section, void *ptr)
 {
 	unsigned char	*p;
 	unsigned int	i;
@@ -42,38 +42,75 @@ static void ft_print_output(struct section_64 *section, void *ptr)
 void		handler_64(char *ptr)
 {
 	unsigned int				i;
-	unsigned int				ncmds;
-	struct mach_header_64		*header;
 	struct segment_command_64	*seg;
 	struct section_64			*sect;
 	struct load_command			*lc;
 
 	i = 0;
-	header = (struct mach_header_64 *) ptr;
-	ncmds = header->ncmds;
-	lc = (void *) ptr + sizeof(*header);
-	while (++i < ncmds)
-	{
+	lc = (void *) ptr + sizeof(struct mach_header_64);
+	while (++i < ((struct mach_header_64 *)ptr)->ncmds)
 		if (lc->cmd == LC_SEGMENT_64)
 		{
 			seg = (struct segment_command_64 *) lc;
-			if (!strcmp(seg->segname, SEG_TEXT))
+			if (!strcmp(seg->segname, SEG_TEXT) && (i = -1))
 			{
-				i = -1;
 				sect = (void *)seg + sizeof(struct segment_command_64);
 				while (++i < seg->nsects)
 				{
 					if (!strcmp(sect->sectname, SECT_TEXT))
-					{
-						ft_print_output(sect, ptr);
-						break ;
-					}
+						return ft_print_output_64(sect, ptr);
 					sect = (void *)sect + sizeof(*sect);
 				}
 			}
 			lc = (void *)lc + lc->cmdsize;
 		}
+}
+
+static void ft_print_output_32(struct section *section, void *ptr)
+{
+	unsigned char	*p;
+	unsigned int	i;
+	int				j;
+
+	printf("(%s,%s) section\n", section->segname, section->sectname);
+	p = (void *)ptr + section->offset;
+	i = 0;
+
+	while (i < section->size)
+	{
+		printf("%016llx ", (unsigned long long)section->addr + i);
+		j = 17;
+		while (i < section->size && --j)
+			printf("%02hhx ", p[i++]);
+		printf("\n");
 	}
+}
+
+void		handler_32(char *ptr)
+{
+	unsigned int			i;
+	struct segment_command	*seg;
+	struct section			*sect;
+	struct load_command		*lc;
+
+	i = 0;
+	lc = (void *) ptr + sizeof(struct mach_header_64);
+	while (++i < ((struct mach_header *)ptr)->ncmds)
+		if (lc->cmd == LC_SEGMENT)
+		{
+			seg = (struct segment_command *) lc;
+			if (!strcmp(seg->segname, SEG_TEXT) && (i = -1))
+			{
+				sect = (void *)seg + sizeof(struct segment_command_64);
+				while (++i < seg->nsects)
+				{
+					if (!strcmp(sect->sectname, SECT_TEXT))
+						return ft_print_output_32(sect, ptr);
+					sect = (void *)sect + sizeof(*sect);
+				}
+			}
+			lc = (void *)lc + lc->cmdsize;
+		}
 }
 
 void		ft_otool(char *ptr)
@@ -83,6 +120,8 @@ void		ft_otool(char *ptr)
 	magic_number = *(int *) ptr;
 	if (magic_number == MH_MAGIC_64)
 		handler_64(ptr);
+	else if (magic_number == MH_MAGIC)
+		handler_32(ptr);
 }
 
 int 		main(int argc, char **argv)
