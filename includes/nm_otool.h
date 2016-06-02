0d571bfcf351e1a11401de32b4c7b9b31b1f6a69
ftriquet <ftriquet@student.42.fr>
2016-06-02 21:02:57 +0200

    [UPD] header for 32bit

--- includes/nm_otool.h
+++ includes/nm_otool.h
@@ -9,42 +9,50 @@ typedef struct s_type
 	char		*sectname;
 	char		symtype;
 }				t_type;
-/*
-** section.c
-*/
-void		ft_build_section_list(t_slice *list, struct mach_header_64 *header);
 
-void		ft_add_segment_to_list(struct segment_command_64 *segment,
+extern t_type	g_type_table[];
+/*
+ ** section.c
+ */
+void			ft_build_section_list_64(t_slice *list, struct mach_header_64 *header);
+
+void			ft_add_segment_to_list_64(struct segment_command_64 *segment,
+		t_slice *list);
+void			ft_build_section_list_32(t_slice *list, struct mach_header *header);
+void			ft_add_segment_to_list_32(struct segment_command *segment,
 		t_slice *list);
 /*
-** nm.c
-*/
-int		ft_display_symtab_64(struct mach_header_64 *header, int nsyms,
+ ** nm.c
+ */
+int				ft_display_symtab_64(struct mach_header_64 *header, int nsyms,
 		int symoff, int stroff, t_slice *list);
-int		ft_display_symlist_64(struct mach_header_64 *header, t_slice *list);
-int		ft_display_symlist_32(struct mach_header *header, t_slice *list);
-int		ft_display_symtab_32(struct mach_header *header, int nsyms,
+int				ft_display_symlist_64(struct mach_header_64 *header, t_slice *list);
+int				ft_display_symlist_32(struct mach_header *header, t_slice *list);
+int				ft_display_symtab_32(struct mach_header *header, int nsyms,
 		int symoff, int stroff, t_slice *list);
 
 /*
-** main.c
-*/
-void	print_section(void *section);
+ ** main.c
+ */
+void			print_section(void *section);
 
-void	nm(char *ptr);
-
-/*
-** symbols.c
-*/
-void	ft_print_nlist64(char *stringtable, struct nlist_64 *symbol, t_slice *list);
-char	get_type(struct nlist_64 *sym, t_slice *sections);
-char	get_section_type(int n_sect, t_slice *sections);
+void			nm(char *ptr);
 
 /*
-** dump_header.c
-*/
-int		dump_header_64(struct mach_header_64 *header);
+ ** symbols.c
+ */
+void			ft_print_nlist_64(char *stringtable, struct nlist_64 *symbol, t_slice *list);
+char			get_type_64(struct nlist_64 *sym, t_slice *sections);
+char			get_section_type_64(int n_sect, t_slice *sections);
 
-int	dump_header(void *header_ptr);
+void			ft_print_nlist_32(char *stringtable, struct nlist *symbol, t_slice *list);
+char			get_type_32(struct nlist *sym, t_slice *sections);
+char			get_section_type_32(int n_sect, t_slice *sections);
+
+/*
+ ** dump_header.c
+ */
+int				dump_header_64(struct mach_header_64 *header);
+int				dump_header(void *header_ptr);
 
 #endif
