34961c63c51d541ea61dc181788cbbf92cad9297
François Triquet <francois.triquet@recast.ai>
2017-01-15 21:55:22 +0100

    sorting by name now check for equality to sort by address

--- src/sort.c
+++ src/sort.c
@@ -3,8 +3,13 @@
 
 int		alpha_cmp(void *sym1, void *sym2, char *stringtable)
 {
-	return (ft_strcmp(stringtable + (((struct nlist *)sym1)->n_un.n_strx),
-				stringtable + (((struct nlist *)sym2)->n_un.n_strx)));
+	int		comp;
+
+	comp = ft_strcmp(stringtable + (((struct nlist *)sym1)->n_un.n_strx),
+			stringtable + (((struct nlist *)sym2)->n_un.n_strx));
+	if (comp == 0)
+		return (((struct nlist *)sym1)->n_value - ((struct nlist *)sym2)->n_value);
+	return (comp);
 }
 
 int		no_cmp(void *sym1, void *sym2)
